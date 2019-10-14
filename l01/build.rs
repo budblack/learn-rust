39a4e3919e249baabc4231dacb4d3930697ba0d5
budblack <budblack@qq.com>
2019-10-14 13:43:09 +0800

    update.

--- l01/build.rs
+++ l01/build.rs
@@ -16,11 +16,26 @@ fn build_linux_unkonw() {
   // -------------
 }
 
+fn build_darwin_apple() {
+  cc::Build::new()
+    .cpp(true)
+    .flag("-std=c++11")
+    .warnings(false)
+    .include("./lib/hello/darwin")
+    .file("./src/cpp/hello/double.cc")
+    .compile("l01");
+  // -------------
+  println!("cargo:rustc-link-search=native=./lib/hello/darwin");
+  println!("cargo:rustc-link-lib=hello");
+  // -------------
+}
+
 fn main() {
   match env::var("TARGET") {
     Ok(val) => {
       match val.as_str() {
         "x86_64-unknown-linux-gnu" => build_linux_unkonw(),
+        "x86_64-apple-darwin" => build_darwin_apple(),
         &_ => {}
       }
     }
